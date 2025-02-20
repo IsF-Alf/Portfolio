3c60ea195ca6690f3e73601b582eb31e05ca58ba
Isaac Alfonso <ifa24991@gmail.com>
2025-02-20 14:46:44 -0300

    Primeros cambios

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
